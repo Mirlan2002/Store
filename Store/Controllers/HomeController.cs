252544317061b998be2cb89ddadd6b694bc68680
Mirlan <mirlandaniaruulu19@gmail.com>
2021-12-06 10:02:29 +0600

    Add project files.

---
+++ Store/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Store.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;
+
+namespace eStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private ApplicationContext db;
+        IWebHostEnvironment hostingEnvironment;
+
+        public HomeController(ILogger<HomeController> logger, ApplicationContext context, IWebHostEnvironment env)
+        {
+            _logger = logger;
+            db = context;
+            this.hostingEnvironment = env;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await db.Products.ToListAsync());
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product, IFormFile titleImageFile)
+        {
+            if (ModelState.IsValid)
+            {
+                if (titleImageFile != null)
+                {
+                    product.TitleImagePath = titleImageFile.FileName;
+                    using (var stream = new FileStream(Path.Combine(hostingEnvironment.WebRootPath, "img/", titleImageFile.FileName), FileMode.Create))
+                    {
+                        titleImageFile.CopyTo(stream);
+                    }
+                }
+                db.Products.Add(product);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
